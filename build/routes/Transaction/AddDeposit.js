9b14a816e84ba908cd7230033c1c549fca571c8e
jefferson.cintra <jefferson.cintra@portoseguro.com.br>
2023-08-25 08:12:16 -0300

    remove build

--- build/routes/Transaction/AddDeposit.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AddDeposit_1 = require("../../useCases/AddDeposit");
-const AddDeposit = (req, res) => {
-    AddDeposit_1.addDepositController.handle(req, res);
-};
-exports.default = AddDeposit;
--- build/routes/User/Create.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const CreateUser_1 = require("../../useCases/CreateUser");
-const CreateUser = (req, res) => {
-    CreateUser_1.createUserController.handle(req, res);
-};
-exports.default = CreateUser;
