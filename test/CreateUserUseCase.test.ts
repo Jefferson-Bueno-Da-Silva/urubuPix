140456bb6361c22ece82e5ce5ce3efb84fc4db97
Jefferson Bueno Mobile Developer <jeffersonbuenoprofissional@gmail.com>
2023-08-30 19:19:39 -0300

    fix: using jest to mock class

--- test/CreateUserUseCase.test.ts
+++ test/CreateUserUseCase.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it, test } from "@jest/globals";
+import { beforeEach, describe, expect, it, jest, test } from "@jest/globals";
 import { CreateUserUseCase } from "../src/useCases/CreateUser/CreateUserUseCase";
 import { IUserRepository } from "../src/repositories/IUserRepository";
 import { Types } from "mongoose";
@@ -7,42 +7,37 @@ import { ICreateUserDTO } from "../src/useCases/CreateUser/CreateUserDTO";
 
 describe("Create User Use Case", () => {
   let _createUserUseCase: CreateUserUseCase;
-  const mockDatabaseUser: IUser[] = [
-    {
-      _id: "1" as unknown as Types.ObjectId,
-      email: "teste3@teste.com.br",
-      name: "Jefferson Bueno",
-      balance: 0,
-    },
-    {
-      _id: "2" as unknown as Types.ObjectId,
-      email: "teste@teste.com.br",
-      name: "Jefferson Bueno",
-      balance: 0,
-    },
-  ];
+  let _userRepository: IUserRepository;
+
+  const mockDatabaseUser: IUser = {
+    _id: "2" as unknown as Types.ObjectId,
+    email: "teste@teste.com.br",
+    name: "Jefferson Bueno",
+    balance: 0,
+  }
   const mockUserDTO: ICreateUserDTO = {
     email: "teste@teste.com.br",
     name: "Jefferson Bueno",
     balance: 0,
   };
 
-  class MockRepositoryUser implements Partial<IUserRepository> {
-    create(user: ICreateUserDTO): Promise<IUser> {
-      return Promise.resolve(mockDatabaseUser[1]);
-    }
-    exists(email: string): Promise<boolean> {
-      return Promise.resolve(mockDatabaseUser[0].email === email);
-    }
-  }
-
   beforeEach(() => {
-    var _userRepository = new MockRepositoryUser() as IUserRepository;
+    _userRepository = {
+      exists: jest.fn(async () => false),
+      create: jest.fn(async () => mockDatabaseUser),
+      findByEmail: jest.fn(() => {
+        throw new Error("Function not implemented.");
+      }),
+      updateUser: jest.fn(() => {
+        throw new Error("Function not implemented.");
+      })
+    }
+
     _createUserUseCase = new CreateUserUseCase(_userRepository);
   });
 
   it("create new user", async () => {
     const user = await _createUserUseCase.execute(mockUserDTO);
-    expect(user).toBe(mockDatabaseUser[1]);
+    expect(user).toBe(mockDatabaseUser);
   });
 });
